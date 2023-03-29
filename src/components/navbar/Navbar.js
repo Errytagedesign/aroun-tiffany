0f022607f42bd65e82a8dd9daf2ad142c3862c29
Errytagedesign <errytage@gmail.com>
2023-03-29 14:34:45 +0100

    navbar created

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 // Icons
 import { BsXLg } from "react-icons/bs";
 import { BiMenu } from "react-icons/bi";
 
 // Styles
-import { Wrapper, Content, NavBars } from "./Navbar.styles";
-
+import "./Navbar.scss";
 // Images
-import BrandLogo from "../../assets/OmoveLogo.png";
+import BrandLogo from "../../assets/aroun-tiffany-main-logo.png";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -20,51 +19,54 @@ function Navbar() {
   };
 
   return (
-    <Wrapper>
-      <Content className="container">
+    <main className="navbar">
+      <nav className="container content">
         <section className="d-flex col-8 col-lg-3 align-items-start">
-          <Link to="#">
-            <img className="BrandLogo" src={BrandLogo} alt="Omove brand logo" />
+          <Link to="/" className="col-3 col-md-4">
+            <img
+              className="col-12"
+              src={BrandLogo}
+              alt="Aroun and Tiffany logo"
+            />
           </Link>
         </section>
-        <NavBars className="" navbar={navbar}>
+        <NavLink
+          className={
+            navbar
+              ? "navIn NavBars col-12 col-lg-7 "
+              : "navOut NavBars col-12 col-lg-7"
+          }
+        >
           {/* Mobile Logo and Close menu icon */}
-          <section className="d-flex w-sm-100 justify-content-between mb-5">
-            <Link to="#">
-              <img
-                className="mobileNavLogo"
-                src={BrandLogo}
-                alt="Omove brand logo"
-              />
-            </Link>
-            <BsXLg className="closeMenu " onClick={ShowNavbar} />
-          </section>
 
           {/* Nav Items */}
-          <section className="d-flex flex-column flex-lg-row justify-content-between col-12 col-lg-4 p-2 ">
+          <section className="d-flex nav-items flex-column flex-lg-row justify-content-between col-12  p-2 ">
             <Link to="#">
-              <h3 className="navItems ">About</h3>
+              <h3 className="navItems ">Our Story</h3>
             </Link>
             <Link to="#">
-              <h3 className="navItems ">Contact</h3>
+              <h3 className="navItems ">When and Where</h3>
             </Link>
             <Link to="#">
-              <h3 className="navItems ">Become a driver</h3>
+              <h3 className="navItems ">Registry </h3>
+            </Link>
+            <Link to="#">
+              <h3 className="navItems ">RSVP </h3>
             </Link>
           </section>
-          <section className="col-12 col-lg-3">
-            {" "}
-            <button className="Btns">
-              {" "}
-              <Link to="#"> Book Now </Link>
-            </button>
-          </section>
-        </NavBars>
-        <main>
-          <BiMenu className="menuIcon" onClick={ShowNavbar} />
-        </main>
-      </Content>
-    </Wrapper>
+        </NavLink>
+        <div className="menuIcon">
+          {!navbar ? (
+            <BiMenu
+              color={`${!navbar ? "var(--main-color)" : "var(--sec-color)"} `}
+              onClick={ShowNavbar}
+            />
+          ) : (
+            <BsXLg color="var(--sec-color)" onClick={ShowNavbar} />
+          )}
+        </div>
+      </nav>
+    </main>
   );
 }
 
