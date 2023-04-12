1e8f25b7b27054096e9b6c953d14f83d140842a2
Errytagedesign <errytage@gmail.com>
2023-04-12 16:17:31 +0100

    contact page created and updated

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdOutlineCall } from "react-icons/md";
+import { MdEmail, MdOutlineCall } from "react-icons/md";
 import "./Contact.scss";
 function Contact() {
   return (
@@ -27,8 +27,8 @@ function Contact() {
           If you need to reach us for any details PRIOR to the wedding
           ceremonies please use the contact details below:
         </h3>
-        <aside className="d-flex col-8 mt-5 flex-row justify-content-between">
-          <div className="main-btn col-12 col-md-5 d-flex justify-content-center align-items-center">
+        <aside className="d-flex flex-column flex-md-row col-8 mt-5 flex-row justify-content-between">
+          <div className="main-btn col-12 col-md-5 mb-3 mb-md-0 d-flex justify-content-center align-items-center">
             <a
               className="d-flex justify-content-between align-items-center"
               href="tel:215-921-1463 "
@@ -63,34 +63,40 @@ function Contact() {
           If you need to reach us for any details DURING the wedding ceremonies
           please use the contact details below:
         </h3>
-        <aside className="d-flex col-8 mt-5 flex-row justify-content-between">
-          <div className="main-btn col-12 col-md-5 d-flex justify-content-center align-items-center">
-            <a
-              className="d-flex justify-content-between align-items-center"
-              href="tel:215-921-1463 "
-            >
-              {" "}
-              <MdOutlineCall
-                className="me-2"
-                size={30}
-                color="var(--sec-color)"
-              />
-              <h3> Aroun </h3>
-            </a>
+        <aside className="d-flex flex-column flex-md-row maids col-8 mt-5 flex-row justify-content-between">
+          <div className="main-btn d-flex flex-column mb-3 mb-md-0 col-12 col-md-5 d-flex justify-content-center align-items-center">
+            <h4> Lead Bridesmaid:</h4>
+            <h3> Farah Khan </h3>{" "}
+            <div>
+              <a className="call" href="tel:2672549967 ">
+                <MdOutlineCall
+                  className="me-2"
+                  size={30}
+                  color="var(--sec-color)"
+                />
+              </a>
+              <a className="call" href="mailto:tfarah0607@gmail.com ">
+                {" "}
+                <MdEmail className="me-2" size={30} color="var(--sec-color)" />
+              </a>
+            </div>
           </div>
-          <div className="main-btn col-12 col-md-5 d-flex justify-content-center align-items-center">
-            <a
-              className="d-flex justify-content-between align-items-center"
-              href="tel:610-551-8230  "
-            >
-              {" "}
-              <MdOutlineCall
-                className="me-2"
-                size={30}
-                color="var(--sec-color)"
-              />
-              <h3>Tiffany </h3>
-            </a>
+          <div className="main-btn d-flex flex-column col-12 col-md-5 d-flex justify-content-center align-items-center ">
+            <h4> Lead Groomsmen:</h4>
+            <h3> Destin Adeniran </h3>{" "}
+            <div>
+              <a className="call" href="tel:4848686377 ">
+                <MdOutlineCall
+                  className="me-2"
+                  size={30}
+                  color="var(--sec-color)"
+                />
+              </a>
+              <a className="call" href="mailto:destinadeniran@aol.com ">
+                {" "}
+                <MdEmail className="me-2" size={30} color="var(--sec-color)" />
+              </a>
+            </div>
           </div>
         </aside>
       </section>
